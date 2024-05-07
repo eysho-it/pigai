9c29fc950a15e4790cf9c5f18fb5ac32a0e67c0d
eysho-it <102192598+eysho-it@users.noreply.github.com>
2024-05-07 16:23:37 +0200

    Work, work

--- clients/catai/src/utils/dark-mode.ts
+++
@@ -1,7 +0,0 @@
-import {writable} from 'svelte/store';
-
-const storedColorTheme = localStorage.getItem('color-theme');
-const isLocalStorageThemeDark = storedColorTheme ?
-    storedColorTheme === 'dark' :
-    window?.matchMedia?.('(prefers-color-scheme:dark)').matches;
-export const darkMode = writable(isLocalStorageThemeDark);
