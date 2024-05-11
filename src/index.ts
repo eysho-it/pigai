13a68c5b02e988cb0b5c1fbad1f36c37aa9b660c
eysho-it <102192598+eysho-it@users.noreply.github.com>
2024-05-11 16:34:30 +0200

    again

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import Remotepigai from './server/remote/remote-pigai.js';
+import FetchModels from './manage-models/about-models/fetch-models/fetch-models.js';
+import createChat from './manage-models/bind-class/bind-class.js';
+import pigaiDB from './storage/app-db.js';
+import ENV_CONFIG from './storage/config.js';
+
+
+export {
+    Remotepigai,
+    FetchModels,
+    createChat,
+    pigaiDB,
+    ENV_CONFIG as PIGAI_ENV_CONFIG,
+};
