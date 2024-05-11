6910d096493a419b91dff92897bf5d3228d4d420
eysho-it <102192598+eysho-it@users.noreply.github.com>
2024-05-11 18:18:51 +0200

    blö
    blö

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,12 @@
-import RemotePigAI from './server/remote/remote-pigai.js';
+import RemotePigAI from './server/remote/remote-catai.js';
 import FetchModels from './manage-models/about-models/fetch-models.js';
-import createChat from './manage-models/bind-class/bind-class.js';
+import createChat, {getModelPath} from './manage-models/bind-class/bind-class.js';
 import PigAIDB from './storage/app-db.js';
 import ENV_CONFIG from './storage/config.js';
 import {PigAIError} from './errors/PigAIError.js';
+import {initPigAILlama} from './manage-models/bind-class/binds/node-llama-cpp/node-llama-cpp-v2/node-llama-cpp-v2.js';
+
+export * from 'node-llama-cpp';
 
 const downloadModel = FetchModels.simpleDownload;
 
@@ -15,5 +18,6 @@ export {
     PigAIDB,
     getModelPath,
     downloadModel,
+    initPigAILlama,
     ENV_CONFIG as PIGAI_ENV_CONFIG,
 };
