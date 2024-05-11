d1dda0a96d898841036fcf9b2b83adfc8d597f43
eysho-it <102192598+eysho-it@users.noreply.github.com>
2024-05-11 18:23:23 +0200

    kpj

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,9 +4,6 @@ import createChat, {getModelPath} from './manage-models/bind-class/bind-class.js
 import PigAIDB from './storage/app-db.js';
 import ENV_CONFIG from './storage/config.js';
 import {PigAIError} from './errors/PigAIError.js';
-import {initPigAILlama} from './manage-models/bind-class/binds/node-llama-cpp/node-llama-cpp-v2/node-llama-cpp-v2.js';
-
-export * from 'node-llama-cpp';
 
 const downloadModel = FetchModels.simpleDownload;
 
@@ -18,6 +15,5 @@ export {
     PigAIDB,
     getModelPath,
     downloadModel,
-    initPigAILlama,
     ENV_CONFIG as PIGAI_ENV_CONFIG,
 };
